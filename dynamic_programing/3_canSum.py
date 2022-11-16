ce0274afb542e93120c83e03f58059cdc582803b
subhadip1203 <subhadip1203@gmail.com>
2022-11-15 22:59:08 -0800

    how sum memo

--- dynamic_programing/3_canSum.py
+++
@@ -1,5 +0,0 @@
-
-def canSum (num , arr):
-    return(num , arr)
-
-print(canSum(7,[5,3,4,7]))
--- dynamic_programing/5_how_sum.py
+++ dynamic_programing/5_how_sum.py
@@ -7,10 +7,10 @@ def howSum(num, arr, memo={}):
     for x in arr:
         reminder = num-x
         reminderResult = howSum(reminder,arr,memo)
+        memo[reminder] = reminderResult
         if reminderResult != None:
             reminderResult.append(x)
-            memo[reminder] = reminderResult
             return reminderResult
     return None
 
-print(howSum(7,[3,5,2]))
+print(howSum(300,[3,5,2]))
