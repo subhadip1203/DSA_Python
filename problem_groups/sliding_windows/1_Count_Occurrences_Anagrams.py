d33b9c7e983816841cffca6661e42e2e1bdbffaf
subhadip1203 <subhadip1203@gmail.com>
2023-06-08 09:19:16 -0700

    sliding windows

--- problem_groups/sliding_windows/1_Count_Occurrences_Anagrams.py
+++ problem_groups/sliding_windows/1_Count_Occurrences_Anagrams.py
@@ -21,7 +21,7 @@ in txt.
 
 def check_Empty_Dict(my_dict):
     for x in my_dict:
-        if my_dict[x] > 0 :
+        if my_dict[x] != 0 :
             return False
     return True
 
@@ -43,6 +43,23 @@ def search(pat, txt):
 
     if check_Empty_Dict(pat_dict) :
         answer = answer+1
+
+    while end < len(txt):
+
+        start_char = txt[start]
+        if start_char in pat_dict:
+            pat_dict[start_char] = pat_dict[start_char] - 1
+        start =start+1
+
+        end_char = txt[end]
+        if end_char in pat_dict:
+            pat_dict[end_char] = pat_dict[end_char] + 1
+        end =end+1
+
+        if check_Empty_Dict(pat_dict) :
+            answer = answer+1
+    
+    return answer
 
 
 
