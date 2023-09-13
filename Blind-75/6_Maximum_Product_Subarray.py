c395fd8fe20905d0db9b4ed349d1b8a2a2ed94d1
subhadip1203 <subhadip1203@gmail.com>
2023-09-12 18:21:24 -0700

    three sum

--- Blind-75/6_Maximum_Product_Subarray.py
+++ Blind-75/6_Maximum_Product_Subarray.py
@@ -14,30 +14,54 @@ The test cases are generated so that the answer will fit in a 32-bit integer.
 youtube : https://www.youtube.com/watch?v=Y6B-7ZctiW8
 '''
 
-def maxProduct( nums ):
-    if len(nums) == 0:
-        return 0
-    else:
-        leftProduct = nums[0]
-        rightProduct = nums[len(nums)-1]
-        result = max(leftProduct, rightProduct)
-        i =1
-        while i < len(nums):
-            leftIndex = i
-            rightIndex = len(nums) - (1+i)
+# def maxProduct( nums ):
+#     if len(nums) == 0:
+#         return 0
+#     else:
+#         leftProduct = nums[0]
+#         rightProduct = nums[len(nums)-1]
+#         result = max(leftProduct, rightProduct)
+#         i =1
+#         while i < len(nums):
+#             leftIndex = i
+#             rightIndex = len(nums) - (1+i)
 
-            # if old value of leftProduct or rightProduct  is 0 , change them to 1
-            leftProduct = 1 if leftProduct == 0 else leftProduct
-            rightProduct = 1 if rightProduct == 0  else rightProduct
+#             # if old value of leftProduct or rightProduct  is 0 , change them to 1
+#             leftProduct = 1 if leftProduct == 0 else leftProduct
+#             rightProduct = 1 if rightProduct == 0  else rightProduct
             
-            leftProduct *= nums[leftIndex]
-            rightProduct *= nums[rightIndex]
+#             leftProduct *= nums[leftIndex]
+#             rightProduct *= nums[rightIndex]
 
              
-            result = max(result ,leftProduct , rightProduct)
-            i+= 1
+#             result = max(result ,leftProduct , rightProduct)
+#             i+= 1
 
-        return result
+#         return result
+
+
+"""
+Youtube : https://www.youtube.com/watch?v=_i4Yxeh5ceQ&list=PLHA0057ByKwKpRHD_WSqJ_dxrHt-Po2sk&index=13&t=7053s
+"""
+
+def maxProduct( nums ):
+    # for multiplication 1 is neutral number
+    result = nums[0]
+    temp_max = nums[0]
+    temp_min = nums[0]
+    for i in range (1,len(nums)):
+        #
+        # max will be either of of those :
+        # 1. lastMax * currentnumber  - for positive numbers
+        # 2. lastMin * currentNumber  - for negative numbers
+        # 3. if last number was 0 , then - only the current number
+        #
+        temp_max = max(temp_max* nums[i] , temp_min* nums[i] , nums[i])
+        temp_min = min(temp_max* nums[i] , temp_min* nums[i] , nums[i] )
+        # print(nums[i], temp_max)
+        result = max(temp_max,result)
+        
+    return result
 
 nums = [2,3,-2,4]
 print(maxProduct(nums))
